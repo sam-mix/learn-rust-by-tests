e7f133701bd0ec17422053bd29dbd7e026526ef4
Sam Mix <1045262335@qq.com>
2022-11-22 13:32:47 +0800

    mess

--- src/lib.rs
+++ src/lib.rs
@@ -87,4 +87,32 @@ mod tests {
         }
     }
 
+
+    #[test]
+    #[should_panic]
+    fn float_eq_panic() {
+        // assert!(0.1+0.2 == 0.3);
+        assert_eq!(0.1+0.2, 0.3);
+    }
+
+    #[test]
+    #[should_panic]
+    fn float_test_1() {
+        let abc: (f32, f32, f32) = (0.1, 0.2, 0.3);
+    let xyz: (f64, f64, f64) = (0.1, 0.2, 0.3);
+
+    println!("abc (f32)");
+    println!("   0.1 + 0.2: {:x}", (abc.0 + abc.1).to_bits());
+    println!("         0.3: {:x}", (abc.2).to_bits());
+    println!();
+
+    println!("xyz (f64)");
+    println!("   0.1 + 0.2: {:x}", (xyz.0 + xyz.1).to_bits());
+    println!("         0.3: {:x}", (xyz.2).to_bits());
+    println!();
+
+    assert!(abc.0 + abc.1 == abc.2);
+    assert!(xyz.0 + xyz.1 == xyz.2);
+    }
+
 }
